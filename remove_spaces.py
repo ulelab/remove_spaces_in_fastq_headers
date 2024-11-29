b407582da272b215d7a7c08a6facb6d91bc9bae1
Charlotte Capitanchik <CharlotteAnne@users.noreply.github.com>
2024-11-29 16:47:08 +0000

    Update remove_spaces.py
    correct encoding

--- remove_spaces.py
+++ remove_spaces.py
@@ -2,24 +2,21 @@
 
 import sys
 import subprocess
-import io
 
 def process_fastq(input_file, output_file):
-    """
-    Process FASTQ file to remove spaces from headers while preserving the rest of the file.
-    Uses pigz for compression/decompression.
-    """
+    """Process FASTQ file to remove spaces from headers using pigz."""
     is_gzipped = input_file.endswith('.gz')
     
     # Set up input stream
     if is_gzipped:
         pigz_decompress = subprocess.Popen(['pigz', '-dc', input_file], 
-                                         stdout=subprocess.PIPE, 
-                                         text=True)
-        in_file = io.TextIOWrapper(pigz_decompress.stdout)
+                                         stdout=subprocess.PIPE)
     else:
-        in_file = open(input_file, 'r')
-    
+        in_file = open(input_file, 'rb')
+        pigz_decompress = subprocess.Popen(['cat'], 
+                                         stdin=in_file,
+                                         stdout=subprocess.PIPE)
+
     # Set up output stream
     out_file_name = output_file if output_file else input_file.replace('.fastq', '.cleaned.fastq')
     if not out_file_name.endswith('.gz'):
@@ -27,24 +24,22 @@ def process_fastq(input_file, output_file):
         
     pigz_compress = subprocess.Popen(['pigz', '-c'], 
                                    stdin=subprocess.PIPE, 
-                                   stdout=open(out_file_name, 'wb'),
-                                   text=True)
+                                   stdout=open(out_file_name, 'wb'))
     
     try:
         line_count = 0
-        for line in in_file:
+        for line in pigz_decompress.stdout:
             if line_count % 4 == 0:  # Header line
-                cleaned_header = line.strip().replace(' ', '_') + '\n'
+                cleaned_header = line.decode().strip().replace(' ', '_').encode() + b'\n'
                 pigz_compress.stdin.write(cleaned_header)
             else:
                 pigz_compress.stdin.write(line)
             line_count += 1
             
     finally:
-        if is_gzipped:
-            pigz_decompress.stdout.close()
-            pigz_decompress.wait()
-        else:
+        pigz_decompress.stdout.close()
+        pigz_decompress.wait()
+        if not is_gzipped:
             in_file.close()
         
         pigz_compress.stdin.close()
@@ -52,7 +47,7 @@ def process_fastq(input_file, output_file):
 
 def main():
     if len(sys.argv) < 2:
-        print("Usage: python remove_spaces.py input.fastq(.gz) [output.fastq.gz]")
+        print("Usage: python script.py input.fastq [output.fastq]")
         sys.exit(1)
     
     input_file = sys.argv[1]
@@ -66,3 +61,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
